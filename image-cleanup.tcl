64ec941c81a94f27cd3ffc97ea7ce46f6715f925
ns.hireme <ns.hireme@gmail.com>
2020-11-19 20:45:15 -0500

    Update image-cleanup.tcl

--- image-cleanup.tcl
+++ image-cleanup.tcl
@@ -1,23 +1,23 @@
 proc rename_images {folder destination number_of_images} {
-	foreach image [lsort -dictionary [glob -nocomplain -directory $folder -type f *]] {
-		 set destination_image [file join $destination [incr number_of_images][file extension $image]]
-		 
-		 if {![file exist $destination_image]} {
-			 puts "Source: $image  Destination: $destination_image" 
-			 file rename $image $destination_image
-		 }
+    foreach image [lsort -dictionary [glob -nocomplain -directory $folder -type f *]] {
+        set destination_image [file join $destination [incr number_of_images][file extension $image]]
+	
+	if {![file exist $destination_image]} {
+	    puts "Source: $image  Destination: $destination_image" 
+	    file rename $image $destination_image
 	}
+    }
 }
 
 proc remove_empty_folders {folders} {
-	foreach folder $folders {
-		set folder_length [llength [glob -nocomplain -directory $folder -type f *]]
-	    
-        if {$folder_length eq 0} {
-			puts "Removing $folder"
-			file delete $folder
-	    }
-	} 
+    foreach folder $folders {
+        set folder_length [llength [glob -nocomplain -directory $folder -type f *]]
+	
+	if {$folder_length eq 0} {
+	    puts "Removing $folder"
+	    file delete $folder
+	}
+    } 
 }
 
 set repository [file join $env(userprofile) "Repository"]
@@ -26,25 +26,24 @@ set destination_parent "R:\Pictures"
 cd $repository
 
 try {
-	
 	# Setup an empty to list to store all the folders we processed. 
 	set empty_folders {}
 	# Using a for loop we create a list of all the folder in the repository that we have to process.
 	foreach folder [lsort -dictionary [glob -type d *]] {
-		set image_path [file join $repository $folder]
-		set destination_child [file join $destination_parent $folder]
+	    set image_path [file join $repository $folder]
+	    set destination_child [file join $destination_parent $folder]
 		
-		# Create a destination directory to store all our renamed images. 
-		file mkdir $destination_child 
-		# Get the number of images from the source. 
-		set number_of_images [llength [glob -nocomplain -directory $destination_child -type f *]]
+	    # Create a destination directory to store all our renamed images. 
+	    file mkdir $destination_child 
+	    # Get the number of images from the source. 
+	    set number_of_images [llength [glob -nocomplain -directory $destination_child -type f *]]
 		
-		# Calling rename_images and passing the subfolders in the repository. 
-		rename_images $folder $destination_child $number_of_images
+	    # Calling rename_images and passing the subfolders in the repository. 
+	    rename_images $folder $destination_child $number_of_images
 	
-		set number_images_source [llength [glob -nocomplain -directory $folder -type f *]]
-		# When the wor is done, we append the folder the the list.
-		lappend empty_folders $folder
+            set number_images_source [llength [glob -nocomplain -directory $folder -type f *]]
+	    # When the wor is done, we append the folder the the list.
+	    lappend empty_folders $folder
 	}
 	# When the work is done, we can remove all the empty folders. 
 	remove_empty_folders $empty_folders 
